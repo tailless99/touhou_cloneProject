3646d04a84352e249703fae8ec24f67b022b7003
ldy <lte6069@naver.com>
2024-01-09 14:52:35 +0900

    레이캐스트 테스트

---
+++ Touhou_CloneProject_Unity/Assets/02.Script/Scencer/TouchingDirections.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchingDirections : MonoBehaviour
+{
+    // 감지 거리
+    [SerializeField] public float rayDistance = 0.2f;
+
+    private bool _IsTouchingTop;
+    private bool _IsTouchingDown;
+    private bool _IsTouchingRight;
+    private bool _IsTouchingLeft;
+
+    public bool IsTouchingTop
+    {  get { return _IsTouchingTop; }
+       set { _IsTouchingTop = value; } 
+    }
+
+    public bool IsTouchingDown
+    {
+        get { return _IsTouchingDown; }
+        set { _IsTouchingDown = value; }
+    }
+
+    public bool IsTouchingRight
+    {
+        get { return _IsTouchingRight; }
+        set { _IsTouchingRight = value; }
+    }
+
+    public bool IsTouchingLeft
+    {
+        get { return _IsTouchingLeft; }
+        set { _IsTouchingLeft = value; }
+    }
+
+    BoxCollider2D boxCollider;
+    public ContactFilter2D castFilter;
+    RaycastHit2D[] topHits = new RaycastHit2D[3];
+    RaycastHit2D[] downHits = new RaycastHit2D[3];
+    RaycastHit2D[] rightHits = new RaycastHit2D[3];
+    RaycastHit2D[] leftHits = new RaycastHit2D[3];
+
+    private void Awake()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        IsTouchingTop = boxCollider.Cast(Vector2.up, castFilter, topHits, rayDistance) > 0 ? true : false;
+        IsTouchingDown = boxCollider.Cast(Vector2.down, castFilter, downHits, rayDistance) > 0 ? true : false;
+        IsTouchingRight = boxCollider.Cast(Vector2.right, castFilter, rightHits, rayDistance) > 0 ? true : false;
+        IsTouchingLeft = boxCollider.Cast(Vector2.left, castFilter, leftHits, rayDistance) > 0 ? true : false;
+
+        Debug.Log(boxCollider.Cast(Vector2.left, castFilter, leftHits, rayDistance));
+    }
+}
--- Touhou_CloneProject_Unity/Assets/02.Script/player/player.cs
+++ Touhou_CloneProject_Unity/Assets/02.Script/player/player.cs
@@ -14,24 +14,50 @@ public class player : MonoBehaviour
     Vector2 moveVector2 = Vector2.zero;
     Rigidbody2D rb;
     Animator animator;
+    TouchingDirections touchingDirections;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        touchingDirections = GetComponent<TouchingDirections>();
     }
 
     private void FixedUpdate()
     {
         // 매 프레임마다 포지션변경
         rb.velocity = new Vector2(moveVector2.x * moveSpeed, moveVector2.y * moveSpeed);
+        Debug.DrawRay(transform.position, Vector2.left * touchingDirections.rayDistance, Color.red);
     }
 
     public void OnMove(InputAction.CallbackContext context)
     {
+        // 백터값 임시 저장 변수
+        Vector2 tempVector = context.ReadValue<Vector2>();
+
+        if(touchingDirections.IsTouchingLeft) {
+            // 왼쪽에 벽이 있을 경우, 왼쪽으로 가려는 값이 있다면 0으로 변경
+            if(tempVector.x < 0) tempVector.x = 0;
+            Debug.Log("닿았음!");
+        }
+        else if (touchingDirections.IsTouchingRight)
+        {
+            // 오른쪽에 벽이 있을 경우, 오른쪽으로 가려는 값이 있다면 0으로 변경
+            if (tempVector.x > 0) tempVector.x = 0;
+        }else if (touchingDirections.IsTouchingTop)
+        {
+            // 위쪽에 벽이 있을 경우, 위쪽으로 가려는 값이 있다면 0으로 변경
+            if (tempVector.y > 0) tempVector.y = 0;
+        }else if (touchingDirections.IsTouchingDown)
+        {
+            // 아래쪽에 벽이 있을 경우, 아래쪽으로 가려는 값이 있다면 0으로 변경
+            if (tempVector.y < 0) tempVector.y = 0;
+        }
+
+        moveVector2 = tempVector;
         // 입력받은 키 값을 Vector2 객체에 대입하여 포지션(위치) 변경
-        moveVector2 = context.ReadValue<Vector2>();
+        //moveVector2 = context.ReadValue<Vector2>();
         DirectionCheck(moveVector2);
     }
 
@@ -45,12 +71,12 @@ public class player : MonoBehaviour
         animator.SetBool(AnimationStrings.IsLeft, leftCheck);
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        // Border 경계에 충돌할 때는 멈추기
-        if (collision.gameObject.tag == "Border")
-        {
-            moveVector2 = Vector2.zero;
-        }
-    }
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    // Border 경계에 충돌할 때는 멈추기
+    //    if (collision.gameObject.tag == "Border")
+    //    {
+    //        moveVector2 = Vector2.zero;
+    //    }
+    //}
 }
